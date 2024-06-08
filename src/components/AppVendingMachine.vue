964567cd3be5c2809499343e255e0b22200d18ec
jerwin <jerwin.mathew@gmail.com>
2024-06-08 23:54:53 +0800

    feat: create main vending machine transaction logic

--- src/components/AppVendingMachine.vue
+++ src/components/AppVendingMachine.vue
@@ -15,7 +15,10 @@
           @update:selectedCategory="updateSelectedCategory"
         />
         <!-- products container -->
-        <ProductsContainer :products="productsData.products" />
+        <ProductsContainer
+          @productSelected="handleProductSelected"
+          :products="productsData.products"
+        />
       </div>
     </main>
 
@@ -23,18 +26,16 @@
       class="px-2 md:px-5 py-10 h-14 bg-white bg-cover bg-center bg-no-repeat flex justify-between items-center"
     >
       <section>
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Cancel
-        </button>
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Done
-        </button>
+        <button class="primary-button">Done</button>
       </section>
     </footer>
+    <!-- add to cart modal -->
+    <Modal
+      v-if="modalProduct"
+      :product="modalProduct"
+      @close="closeModal"
+      @add-to-cart="addToCart"
+    />
   </div>
 </template>
 
@@ -42,8 +43,47 @@
 import { onMounted, reactive, ref } from "vue";
 import CategoriesNav from "@/components/VendingMachine/CategoriesNav.vue";
 import ProductsContainer from "@/components/VendingMachine/ProductsContainer.vue";
+import Modal from "@/components/VendingMachine/ModalAddToCart.vue";
 import type { Category, Product } from "@/types/types";
+import { useCartStore } from "@/stores/cart";
 
+//MAIN FUNCTIONALITY
+function getChange(bill: number, owed: number): Record<string, number> {
+  const denominations = [1000, 500, 200, 100, 50, 20, 10, 5, 1];
+  const change: Record<string, number> = {};
+
+  let remaining = bill - owed;
+
+  denominations.forEach((denomination) => {
+    if (remaining >= denomination) {
+      const count = Math.floor(remaining / denomination);
+      change[`P${denomination}`] = count;
+      remaining -= count * denomination;
+    }
+  });
+
+  return change;
+}
+
+// Test cases
+// console.log(getChange(0, 0));
+// console.log(getChange(1000, 1));
+// console.log(getChange(500, 600));
+// console.log(getChange(100, 27));
+
+const cartStore = useCartStore();
+
+const addToCart = (product: Product, quantity: number) => {
+  cartStore.addToCart(product, quantity); // call addToCart action from the cart store
+};
+
+const removeFromCart = (index: number) => {
+  cartStore.removeFromCart(index); // call removeFromCart action from the cart store
+};
+
+const clearCart = () => {
+  cartStore.clearCart(); // call clearCart action from the cart store
+};
 type ProductsData = {
   categories: Category[] | null;
   products: Product[] | null;
@@ -55,6 +95,14 @@ const productsData = reactive<ProductsData>({
 });
 
 const selectedCategory = ref(1);
+const modalProduct = ref<Product | null>(null);
+const openModal = (product: Product) => {
+  modalProduct.value = product;
+};
+
+const closeModal = () => {
+  modalProduct.value = null;
+};
 
 const fetchCategories = async () => {
   const response = await fetch("data.json");
@@ -77,6 +125,9 @@ const updateSelectedCategory = (categoryId: number) => {
   fetchProducts(categoryId);
 };
 
+const handleProductSelected = (product: Product) => {
+  openModal(product);
+};
 onMounted(() => {
   fetchCategories();
   fetchProducts(selectedCategory.value);
