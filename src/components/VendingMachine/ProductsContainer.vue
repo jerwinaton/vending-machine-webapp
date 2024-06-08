618beb11d67561638683aecc060fd70f4dd64288
jerwin <jerwin.mathew@gmail.com>
2024-06-08 23:04:54 +0800

    fix: add alt for product

--- src/components/VendingMachine/ProductsContainer.vue
+++ src/components/VendingMachine/ProductsContainer.vue
@@ -5,9 +5,16 @@
     <ul class="grid grid-cols-2 sm:grid-cols-3 gap-3 max-h-full">
       <li v-for="product in products" :key="product.id">
         <article
+          tabindex="0"
+          role="button"
+          @click="handleProductClick(product)"
           class="bg-white h-full p-2 rounded-xl shadow flex flex-col items-center"
         >
-          <img class="w-full h-full" src="/src/assets/logo.svg" alt="" />
+          <img
+            class="w-full h-full"
+            src="/src/assets/logo.svg"
+            :alt="product.name"
+          />
           <p class="font-bold">
             {{ formatAsMoney(product.price) }}
           </p>
@@ -24,6 +31,12 @@ import type { Product } from "@/types/types";
 defineProps({
   products: Array as () => Product[] | null,
 });
+
+const emit = defineEmits(["productSelected"]);
+
+const handleProductClick = (product: Product) => {
+  emit("productSelected", product);
+};
 
 const formatAsMoney = (amount: number) => {
   return new Intl.NumberFormat("en-PH", {
